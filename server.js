b539d2299f356c883b652b7ef1a7e87fd410f65c
DANIEL COSTA CUNHA <daniel.cunha@itbam.org.br>
2020-10-14 16:15:33 -0400

    conexao com mongodb cloud

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+const uri = 'mongodb+srv://cunhacdaniel:Maodcc!@@clusterinit.htyik.mongodb.net/mongo-node?retryWrites=true&w=majority';
+//mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+.then( () => {
+    console.log("MongoDB Connected");
+})
+.catch( err  => console.log(err));
+
 
 requireDir('./src/models');
 
